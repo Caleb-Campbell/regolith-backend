2de6513e68f8e32bb6eccfb1854884bffa9e7162
Caleb Campbell <calebcampbellcrm@gmail.com>
2022-12-06 10:07:14 -0700

    wip

--- api/userRouter.js
+++ api/userRouter.js
@@ -1,7 +1,14 @@
 const router = require('express').Router()
+const Users = require('./models/users')
 
-router.get('/', (req, res) => {
-    res.status(200).json({message: 'you got the users and then some!'})
+router.get('/', async (req, res) => {
+    try{
+        const users = await Users.find()
+        res.status(200).json(users)
+    }
+    catch (err) {
+        res.status(500).json({message: err.message})
+    }
 })
 
 
